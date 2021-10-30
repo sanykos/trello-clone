fd345d1afbb28b735b6356379e08a0856057d208
Alexander Kosykh <sanykos446@gmail.com>
2021-10-30 15:35:27 +0300

    Add useFocus

--- src/components/NewItemFormContainer/index.tsx
+++ src/components/NewItemFormContainer/index.tsx
@@ -1,12 +1,18 @@
 import { useState } from 'react';
+import { useFocus } from '../../utils/useFocus';
 import { NewItemFormProps } from './intefaces';
 import { NewItemFormContainerStyle, NewItemInputStyle, NewItemButtonStyle } from './styles';
 
 export const NewItemFormContainer = ({ onAdd }: NewItemFormProps) => {
     const [text, setText] = useState('');
+    const inputRef = useFocus();
     return (
         <NewItemFormContainerStyle>
-            <NewItemInputStyle value={text} onChange={(e) => setText(e.target.value)} />
+            <NewItemInputStyle
+                ref={inputRef}
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+            />
             <NewItemButtonStyle onClick={() => onAdd(text)}>Create</NewItemButtonStyle>
         </NewItemFormContainerStyle>
     );

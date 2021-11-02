7e945c49a87223875f3d2f3c6faeafcc62d2595a
Alexander Kosykh <sanykos446@gmail.com>
2021-11-02 20:14:08 +0300

    add handleAddText

--- src/components/NewItemFormContainer/index.tsx
+++ src/components/NewItemFormContainer/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { useFocus } from '../../utils/useFocus';
 import { NewItemFormProps } from './intefaces';
 import { NewItemFormContainerStyle, NewItemInputStyle, NewItemButtonStyle } from './styles';
@@ -6,12 +6,20 @@ import { NewItemFormContainerStyle, NewItemInputStyle, NewItemButtonStyle } from
 export const NewItemFormContainer = ({ onAdd }: NewItemFormProps) => {
     const [text, setText] = useState('');
     const inputRef = useFocus();
+
+    const handleAddText = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            onAdd(text);
+        }
+    };
+
     return (
         <NewItemFormContainerStyle>
             <NewItemInputStyle
                 ref={inputRef}
                 value={text}
                 onChange={(e) => setText(e.target.value)}
+                onKeyPress={handleAddText}
             />
             <NewItemButtonStyle onClick={() => onAdd(text)}>Create</NewItemButtonStyle>
         </NewItemFormContainerStyle>
